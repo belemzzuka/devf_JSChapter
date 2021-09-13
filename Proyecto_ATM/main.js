ab513058430062d2c2927ba36cfa316085597c8f
Belem Garrido <belem.garrido@gmail.com>
2021-09-12 23:13:18 -0500

    agregar funcionalidades de deposito y retiro de dinero al ATM

--- Proyecto_ATM/main.js
+++ Proyecto_ATM/main.js
@@ -62,12 +62,88 @@ var currentUser = {};
 
 //OPERACIONES BANCARIAS
 
+let mensajesConfirmacion = document.getElementById("mensajes-confirmacion");
+let depositarCantidad = document.getElementById("depositar");
+depositarCantidad.hidden = true;
+let retirarCantidad = document.getElementById("retirar");
+retirarCantidad.hidden = true;
+
+//CONSULTAR SALDO
 function consultarSaldo(usuario){
-    let saldo = document.getElementById("saldo");
-    saldo.innerText = `Tu saldo es ${usuario.saldo}`;
+    mensajesConfirmacion.innerText = `Tu saldo es $${usuario.saldo}`;
 };
 
 let btnConsultar = document.getElementById("btnConsultar");
 btnConsultar.addEventListener("click",function(){
+    depositarCantidad.hidden = true;
+    retirarCantidad.hidden = true;
     consultarSaldo(currentUser);
 });
+
+//DEPOSITAR DINERO
+function depositarDinero(usuario, cantidad){
+    let deposito = usuario.saldo + cantidad;
+    if (deposito <= 990){
+        for(let i=0; i<cuentas.length; i++){
+            if(usuario.nombre===cuentas[i].nombre){
+                cuentas[i].saldo += cantidad;
+            }
+        }
+        depositarCantidad.hidden = true;
+        mensajesConfirmacion.innerText = `La cantidad depositada fue $${cantidad}.
+        Tu nuevo saldo es $${usuario.saldo}.`;
+    } else {
+        mensajesConfirmacion.innerText = "No puedes depositar esa cantidad, tu saldo no debe ser mayor a $990";
+    }
+}
+
+let btnDepositar = document.getElementById("btnDepositar");
+btnDepositar.addEventListener("click",function(){
+    mensajesConfirmacion.innerText = "";
+    retirarCantidad.hidden = true;
+    let emptyInput = document.getElementById("txt-cantidad-depositar")
+    emptyInput.value = 0;
+    depositarCantidad.hidden = false;
+});
+
+let btnDepositarCantidad = document.getElementById("btn-depositar-cantidad");
+btnDepositarCantidad.addEventListener("click",function(){
+    let cantidad = parseInt(document.getElementById("txt-cantidad-depositar").value);
+    depositarCantidad.hidden = true;
+    depositarDinero(currentUser, cantidad);
+});
+
+
+//RETIRAR DINERO
+function retirarDinero(usuario, cantidad){
+    let retiro = usuario.saldo - cantidad;
+    if (retiro >= 10){
+        for(let i=0; i<cuentas.length; i++){
+            if(usuario.nombre===cuentas[i].nombre){
+                cuentas[i].saldo -= cantidad;
+            }
+        }
+        retirarCantidad.hidden = true;
+        mensajesConfirmacion.innerText = `La cantidad retirada fue $${cantidad}.
+        Tu nuevo saldo es $${usuario.saldo}.`;
+    } else {
+        mensajesConfirmacion.innerText = "No puedes retirar esa cantidad, tu saldo es insuficiente";
+    }
+}
+
+let btnRetirar = document.getElementById("btnRetirar");
+btnRetirar.addEventListener("click",function(){
+    mensajesConfirmacion.innerText = "";
+    depositarCantidad.hidden = true;
+    let emptyInput = document.getElementById("txt-cantidad-retirar")
+    emptyInput.value = 0;
+    retirarCantidad.hidden = false;
+});
+
+let btnRetirarCantidad = document.getElementById("btn-retirar-cantidad");
+btnRetirarCantidad.addEventListener("click",function(){
+    let cantidad = parseInt(document.getElementById("txt-cantidad-retirar").value);
+    retirarCantidad.hidden = true;
+    retirarDinero(currentUser, cantidad);
+});
+
